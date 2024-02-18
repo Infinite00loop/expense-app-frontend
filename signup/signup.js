13915d2b93ea7a299ab88a2d2c41dbeeb4ddfb90
Infinite00loop <samriddhisingh07092002@gmail.com>
2024-02-18 12:05:45 +0530

    frontend

--- signup/signup.js
+++ signup/signup.js
@@ -25,19 +25,3 @@ function onsignup(){
         }
        
 }
-function onlogin(){
-    var email_=document.getElementById('idx1').value;
-    var password_=document.getElementById('idx2').value;
-   
-    let myObj={
-        email: email_,
-        password: password_
-    };
-        if(email_!='' && password_!='' ){
-            axios.post('http://localhost:5000/login-user',myObj)
-            .then((res)=> alert(res.data))
-            .catch((err)=> alert(err.response.data));
-           
-        }
-       
-}
